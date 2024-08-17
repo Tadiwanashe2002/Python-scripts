25b6c1196922669495961c008142d302047b89a9
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2024-08-17 12:21:45 +0200

    Update area_of_a_rectangle.py
    I have added clean readability and make it easier to read

--- area_of_a_rectangle.py
+++ area_of_a_rectangle.py
@@ -1,4 +1,4 @@
-length = int(input('Enter your length'))
-width = int(input('Enter youtr width'))
+length = int(input('Enter your length : '))
+width = int(input('Enter youtr width : '))
 area = length*width
 print(f'area of rectangle with {width} and {length} is{area} ')
